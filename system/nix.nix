1057310bd81008729a02e31d61dba365e7e5c6d5
Chloe Bethel <chloe@9net.org>
2024-01-09 11:47:31 +0000

    add unstable maybe

--- system/nix.nix
+++ system/nix.nix
@@ -1,7 +1,7 @@
-{ commonDir, ... }: {
+{ commonDir, unstable, ... }: {
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
   };
   nixpkgs.config = import (commonDir + "/nixpkgs/config.nix");
-  nixpkgs.overlays = import (commonDir + "/nixpkgs/overlays.nix");
+  nixpkgs.overlays = import (commonDir + "/nixpkgs/overlays.nix") { inherit unstable; here = commonDir + "/common/nixpkgs"; };
 }
