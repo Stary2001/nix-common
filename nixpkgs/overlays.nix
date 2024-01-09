d8b8da417aa612199230d9dc613fffda73f6e025
Chloe Bethel <chloe@9net.org>
2024-01-09 11:40:42 +0000

    oops

--- nixpkgs/overlays.nix
+++ nixpkgs/overlays.nix
@@ -3,6 +3,6 @@
     # Add packages
     lightspeed-ingest = super.callPackage ./packages/lightspeed-ingest.nix {};
     lightspeed-webrtc = super.callPackage ./packages/lightspeed-webrtc.nix {};
-    lightspeed-frontend = super.callPackage ./packages/lightspeed-frontend.nix {}
+    lightspeed-frontend = super.callPackage ./packages/lightspeed-frontend.nix {};
   })
 ]
