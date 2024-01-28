994d6165a0813bcf86e453fd557b908b114eb5dc
Chloe Bethel <chloe@9net.org>
2024-01-28 11:52:31 +0000

    make a base module with stuff i really want

--- flake.nix
+++ flake.nix
@@ -7,34 +7,24 @@
   {
       homeModules = {
         base = {pkgs, ...}: {
-          imports = [./user/base.nix];
-        };
-        nix = {pkgs, ...}: {
-          _module.args.commonDir = ./.;
-          imports = [./user/nix.nix];
+          imports = [./user/base.nix ./user/nix.nix];
         };
       };
 
       nixosModules = {
+        base = {pkgs, ...}: {
+          _module.args.commonDir = ./.;
+          imports = [./system/locale.nix ./system/ssh-keys.nix ./system/nix.nix];
+        };
         nine-net = {pkgs, ...}: {
           imports = [./system/9net.nix];
         };
         avahi = {pkgs, ...}: {
           imports = [./system/avahi.nix];
         };
-        locale = {pkgs, ...}: {
-          imports = [./system/locale.nix];
-        };
-        ssh-keys = {pkgs, ...}: {
-          imports = [./system/ssh-keys.nix];
-        };
         wait-online-any = {...}: {
           systemd.network.wait-online.anyInterface = true;
 	};
-        nix = {...}: {
-          _module.args.commonDir = ./.;
-          imports = [./system/nix.nix];
-        };
         service-lightspeed = { pkgs, ... }: {
           imports = [./system/service/lightspeed.nix];
         };
