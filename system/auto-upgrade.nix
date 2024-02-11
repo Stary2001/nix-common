522cdc184954310cdc2c8e6cbc8b0f7c0b88c6c2
Chloe Bethel <chloe@9net.org>
2024-02-11 15:37:54 +0000

    Try to pull

--- system/auto-upgrade.nix
+++ system/auto-upgrade.nix
@@ -1,8 +1,13 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   system.autoUpgrade = {
     enable = true;
     allowReboot = true;
     flake = "/home/stary/sys-${config.networking.hostName}";
     flags = [];
+  };
+  systemd.services.nixos-upgrade = {
+    preStart = '''
+      ${pkgs.sudo}/bin/sudo -u stary ${pkgs.git}/bin/git pull --rebase
+    ''';
   };
 }
